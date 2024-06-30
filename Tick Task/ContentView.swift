0940030bf6e9a1de3c767717f267a3324398ccbf
Yusuf <ybkocaman@gmail.com>
2024-06-30 09:12:10 +0100

    Initial Commit

---
+++ Tick Task/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tick Task
+//
+//  Created by Yusuf Burak on 30/06/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tick Task/Tick_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tick_TaskApp.swift
+//  Tick Task
+//
+//  Created by Yusuf Burak on 30/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct Tick_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
